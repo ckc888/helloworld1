bb9ee29e7aaff780bccb9edc7a60b8e192789d92
ckc888 <1399116862@qq.com>
2024-06-04 15:14:11 +0800

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello{
+	public static void main(String[] args){
+	      System.out.println("你好，java！");
+	}
+	
+}
+class test{
+		public static void main(String[] args){
+			System.out.println("你好，非public的另外的一个类！");
+		}	
+  }
---
+++ test1.java
@@ -0,0 +1,16 @@
+public class test1{
+	/**
+	 *
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 **/
+
+	public static void main(String[] args){
+
+	   System.out.println("书名\t作者\t价格\t销量");
+	   System.out.println("三国\t罗贯中\t120\t1000");
+	}
+}
